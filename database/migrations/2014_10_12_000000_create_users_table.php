db840295a4cc358ac03decab6a056c407905c387
Sadia Akter <sadiaakterwork@gmail.com>
2020-05-13 12:45:13 +0600

    userTable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name',128);
+            $table->string('first_name',128);
+            $table->string('last_name',128)->nullable();
+            $table->string('username',128)->unique();
             $table->string('email',128)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('email_verification_token',88)->nullable();
             $table->string('password',128);
-            $table->string('phone_number',32);
+            $table->string('phone_number',32)->unique();
+
+            $table->string('street_address');
+            $table->unsignedTinyInteger('division_id')->comment('Division Table ID');
+            $table->unsignedTinyInteger('district_id')->comment('District Table ID');
+
+            $table->unsignedTinyInteger('status')->default(1)->comment('0=In-Active|1=Active| 2=Ban');
+            $table->string('ip_address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('shipping_address')->nullable();
+
 //            $table->bigInteger('reward_points')->default(0);
 //            $table->string('facebook_id',32)->nullable();
 //            $table->string('google_id',32)->nullable();
